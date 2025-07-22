3bf39ef14c40a646ad047a8bded7a1b0c996d7ab
orzu111 <orzumurodsuyunboyev@gmail.com>
2025-07-22 16:09:05 +0500

    finished

---
+++ donyorbek.js
@@ -0,0 +1,12 @@
+
+const arr = [1, 2, 2, 4, 5,]
+Array.prototype.map = function (yy) {
+    for (let i = 0; i < this.length; i++) {
+        yy(this[i], i, this);
+    }
+};
+
+let ozg = ["donyorbek", 'donyorbek'];
+ console.log (ozg. mathXMLDocument;
+
+
